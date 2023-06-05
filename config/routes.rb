d9e256c8cb948a7e4bb71bc582df33d9398bf971
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-06 04:48:10 +0500

    Create root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'root/index'
   namespace :api do
     namespace :v1 do
       resources :greetings, only: [:index]
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'root#index'
 end
