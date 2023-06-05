dfb6c19d2e4b9c25f0f59aea8a5ed26acff1ab5b
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-06 01:34:31 +0500

    Create a table and five different greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create!(message: "Hello world!")
+Greeting.create!(message: "Welcome to my first rails react app")
+Greeting.create!(message: "Welcome to my application")
+Greeting.create!(message: "Hello, how are you?")
+Greeting.create!(message: "Welcome to my rails app")
